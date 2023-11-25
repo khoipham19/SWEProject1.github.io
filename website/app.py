bb28c6d986f6a05f31b25db221748f8ee93824bf
Khoi Pham <khoipham@.edu>
2023-11-25 17:52:14 -0600

    made the merges work together

--- website/app.py
+++ website/app.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, jsonify, render_template
-from functions import get_db, add_ingredient_function, remove_ingredient_function, view_fridge_function
+from flask import Flask, request, jsonify, render_template, Blueprint
 import sqlite3
-
-app = Flask(__name__)
+fridge = Blueprint('fridge', __name__)
 DATABASE = "fridge.db"
 
 def get_db():
@@ -10,39 +8,73 @@ def get_db():
     return conn
 
 # Login page
-@app.route('/')
+@fridge.route('/')
 def login():
     return render_template('login.html')
 
 # Main page; function page; fridge page
-@app.route('/fridge')
+@fridge.route('/fridge')
 def index():
     return render_template('index.html')
 
 # Recipes page
-@app.route('/recipes')
+@fridge.route('/recipes')
 def recipes():
     return render_template('recipes.html')
 
 # Remove the ingredient
-@app.route('/remove', methods = ['POST'])
+@fridge.route('/remove', methods = ['POST'])
 def remove_ingredient():
     data = request.json
-    return remove_ingredient_function(data)
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT quantity FROM ingredients WHERE name=?", (data['name'],))
+    result = cursor.fetchone()
+
+    if result:
+        new_quantity = result[0] - int(data['quantity'])
+        if new_quantity <= 0:
+            cursor.execute("DELETE FROM ingredients WHERE name=?", (data['name'],))
+        else:
+            cursor.execute("UPDATE ingredients SET quantity=? WHERE name=?", (new_quantity, data['name'],))
+    else:
+        return jsonify({"message": "Ingredient not found!"})
+    
+    conn.commit()
+    return jsonify({"message": "Ingredient removed!"})
 
 # Add the ingredient
-@app.route('/add', methods=['POST'])
+@fridge.route('/add', methods=['POST'])
 def add_ingredient():
     data = request.json
-    return add_ingredient_function(data)
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT quantity FROM ingredients WHERE name=?", (data['name'],))
+    result = cursor.fetchone()
+
+    if result:
+        new_quantity = result[0] + int(data['quantity'])
+        cursor.execute("UPDATE ingredients SET quantity=? WHERE name=?", (new_quantity, data['name']))
+
+    else:
+        cursor.execute("INSERT INTO ingredients (name, quantity) VALUES (?, ?)", (data['name'], data['quantity'],))
+
+    conn.commit()
+    return jsonify({"message": "Ingredient added!"})
 
 # View the fridge in list
-@app.route('/view', methods=['GET'])
+@fridge.route('/view', methods=['GET'])
 def view_fridge():
-    return view_fridge_function()
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM ingredients")
+    items = cursor.fetchall()
+    return jsonify(items)
 
 # View avaiable recipes based on available ingredients
-@app.route('/view_recipes', methods=['GET'])
+@fridge.route('/view_recipes', methods=['GET'])
 def view_recipes():
     # Recipes with their required ingredients
     recipes_dict = {
@@ -65,9 +97,4 @@ def view_recipes():
 
     return jsonify(possible_recipes)
 
-if __name__ == "__main__":
-    conn = sqlite3.connect(DATABASE)
-    cursor = conn.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS ingredients (name TEXT, quantity INTEGER)")
-    conn.commit()
-    app.run(debug=True)
+
