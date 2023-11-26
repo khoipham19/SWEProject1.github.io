98589335b780b6810b9a477e752dc2dd591adaa7
MarshallEubanks <Marshall.K.Eubanks-1@ou.edu>
2023-11-25 18:14:53 -0600

    Updated files to correctly route login success to the index page

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,29 @@
 from flask import Flask
+import sqlite3
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'lmaolmaolmaolmao'
-
-    from .views import views
-    from .auth import auth
+    DATABASE = "fridge.db"
     
 
-
+    def initialize_db():
+        conn = sqlite3.connect(DATABASE)
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS ingredients (name TEXT, quantity INTEGER)")
+        conn.commit()
+        conn.close()
+    
+    initialize_db()
+        
+    from .views import views
+    from .auth import auth
+    from .app import fridge
+    
     app.register_blueprint(views, url_prefix ='/')
     app.register_blueprint(auth, url_prefix ='/')
+    app.register_blueprint(fridge, url_prefix ='/')
 
 
     return app
